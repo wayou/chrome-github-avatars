cbd699ec166918ab38bde3fd1a3c06080db1edc3
wayou <liuwayong@gmail.com>
2017-03-05 11:59:58 +0800

    - show avatar for all users in the activity page - show divider line for pagination

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,9 +1,9 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function (details) {
-    console.log('previousVersion', details.previousVersion);
+    // console.log('previousVersion', details.previousVersion);
 });
 
 chrome.tabs.onUpdated.addListener(function (tabId) {
-    chrome.pageAction.show(tabId);
+    // chrome.pageAction.show(tabId);
 });
--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -4,92 +4,95 @@
 // ------------------------------
 var $news = $('.news')
 
-// hacking into jQuery ajax to be notified 
-// once the pagination ajax completed
-, onPaginationComplete = function() {
-    getAvatarsForUsers( function( data ) {
-        printImages( data.items );
-    });
-}
-
-// generate search url for a 
-// list of github usernames
-// ------------------------
-, generateUrl = function( array, pagination ) {
-    for( var i in array ) {
-        array[ i ] = 'user%3A' + array[ i ];
-    }
-    return '//api.github.com/search/users?q=' + array.join( '+' ) + ( pagination ? '&per_page=' + pagination : '' );
-}
- 
-// drop duplicated items from an array
-// -----------------------------------
-, unique = function( array ) {
-    var temp = {};
-    for( var i in array ) {
-        temp[ array[i] ] = {};
-    }
-    return Object.keys( temp );
-}
-
-// get new users
-// -------------
-, prepareNewUsersOnPage = function() {
-    var users = $news.find('.alert').not('.push, .public, .issues_comment, .avatar-ready').find('.title a:eq(0)').map(function() {
-        var $self = $(this)
-        , username = $self.text();
-        
-        $self.addClass(username);
-
-        $self.parent().addClass('avatar-container')
-        
-        // store username
-        $self.closest('.alert')
-            .attr('data-username', username);
-        
-        return username;
-    }).toArray();
-
-    return unique( users );
-}
- 
-// print all users gravatars into DOM
-// ----------------------------------
-, printImages = function( items ) {
-    for( var item in items ) {
-        var $img = $('<img />').addClass('github-avatar').attr( 'src', items[ item ].avatar_url );
-
-        $('.alert:not(.avatar-ready,.issues_opened) .' + items[ item ].login ).each(function() {
-            var $self = $(this);
-
-            $img.clone().insertBefore( $self );
-            $self.closest( '.alert' ).addClass('avatar-ready');
+    // hacking into jQuery ajax to be notified 
+    // once the pagination ajax completed
+    , onPaginationComplete = function () {
+        getAvatarsForUsers(function (data) {
+            printImages(data.items);
         });
     }
-}
 
-, getAvatarsForUsers = function( callback ) {
-    $.get( generateUrl( prepareNewUsersOnPage(), 31 ), callback );
-}
+    // generate search url for a 
+    // list of github usernames
+    // ------------------------
+    , generateUrl = function (array, pagination) {
+        for (var i in array) {
+            array[i] = 'user%3A' + array[i];
+        }
+        return '//api.github.com/search/users?q=' + array.join('+') + (pagination ? '&per_page=' + pagination : '');
+    }
 
-// tricking first element in feed
-// ------------------------------
-, $first = $('.news .alert').eq(0)
-, $clone;
+    // drop duplicated items from an array
+    // -----------------------------------
+    , unique = function (array) {
+        var temp = {};
+        for (var i in array) {
+            temp[array[i]] = {};
+        }
+        return Object.keys(temp);
+    }
+
+    // get new users
+    // -------------
+    , prepareNewUsersOnPage = function () {
+        var $row = $news.find('.alert').not('.push, .public, .issues_comment, .avatar-ready');
+        var users = $row.find('.title a').map(function () {
+            var $self = $(this)
+                , username = $self.text().split('/')[0];
+
+            $self.addClass(username);
+
+            $self.parent().addClass('avatar-container')
+
+            // store username
+            $self.attr('data-username', username);
+
+            return username;
+        }).toArray();
+
+        return unique(users);
+    }
+
+    // print all users gravatars into DOM
+    // ----------------------------------
+    , printImages = function (items) {
+        for (var item in items) {
+            var $img = $('<img />').addClass('github-avatar').attr('src', items[item].avatar_url);
+            $('.alert:not(.issues_opened) .' + items[item].login + ':not(.avatar-ready)').each(function () {
+                var $self = $(this);
+
+                $self.prepend($img.clone());
+                $self.addClass('avatar-ready');
+            });
+        }
+    },
+
+    addDividerLine = function () {
+        $('.alert').last().append($('<hr/>'));
+    }
+
+    , getAvatarsForUsers = function (callback) {
+        $.get(generateUrl(prepareNewUsersOnPage(), 31 * 3), callback);
+    }
+
+    // tricking first element in feed
+    // ------------------------------
+    , $first = $('.news .alert').eq(0)
+    , $clone;
 
 $first.clone().insertBefore($first);
 $clone = $first.prev();
 $first.css('border', 'none');
 $clone.css({
-      height: 0
+    height: 0
     , padding: 0
 });
 
- 
+
 // query github API
 // ----------------
-getAvatarsForUsers( function( data ) { 
-    printImages( data.items ); 
+getAvatarsForUsers(function (data) {
+    printImages(data.items);
 });
 
 // hacking into pagination calls
@@ -97,19 +100,21 @@ getAvatarsForUsers( function( data ) {
 // since we don't have access to github's 
 // jQuery object, we'll do a workaround here
 var buttonSelector = '.js-events-pagination, .ajax-pagination-btn';
-$news.on('click', buttonSelector, function() {
-    
+$news.on('click', buttonSelector, function () {
+
+    addDividerLine();
+
     console.info('waiting for a pagination to complete');
 
-    var id = setInterval(function() {
-        var $button = $( buttonSelector );
-        if( $button.closest( '.loading' ).length === 0 ) {
+    var id = setInterval(function () {
+        var $button = $(buttonSelector);
+        if ($button.closest('.loading').length === 0) {
             clearInterval(id);
             onPaginationComplete();
             console.info('pagination completed');
         } else {
             console.info('waiting...');
         }
-        
+
     }, 500);
 });
